a7bd0b22846982ad1a1e1e2380551b406dcd88e0
EliamHannaUpssitech <eliam.hanna@univ-tlse3.fr>
2025-02-13 10:30:44 +0100

    deroullement de la partie en commentaires

--- src/cartes/Carte.java
+++ src/cartes/Carte.java
@@ -1,5 +1,9 @@
 package cartes;
 
 public class Carte {
+	private String nom;
+	private Effet[] effets = new Effet[3];
+	private int type; // 0 passif ; 1 actif
+	
 	
 }
---
+++ src/jeu/Jeu.java
@@ -0,0 +1,50 @@
+package jeu;
+
+public class Jeu {
+	/*
+	//// Initialisation des joueurs ////
+		Vie = 5
+		Popularite = 0
+		for i in range 5 ( piocheCarte() )
+		banc assigné + 5 places vides
+	
+		pour Joueur 1
+		pour Joueur 2
+	
+	Tour = 0
+	
+	Creation pioche (40 cartes)
+	
+	Zone attaque = vide
+	*/
+	
+	/*
+	//// AFFICHAGE ////
+	
+	print:
+		Contexte du jeu
+		règles
+		présentation des joueurs
+	*/
+	
+	/*
+	//// JEU ////
+	
+	Tour ++
+	if mainJoueur < 5 ( piocheCarte() )
+	
+	print :
+	|	NOMJOUEUR : X Vie / X Popularite
+	|	CARTESMAIN : 1.xxx / 2.xxx / 3.xxx / 4.xxx / 5.xxx
+	|	BANC : 1.xxx / 2.xxx / 3.xxx / 4.xxx / 5.xxx
+	|	
+	|	NOMADV : X Vie / X Popularite
+	|	BANCADV : 1.xxx / 2.xxx / 3.xxx / 4.xxx / 5.xxx
+	|	
+	|	Utilisez une carte (1 à 5) ou passez votre tour (0):
+	|	if int[1;5] ( utiliserCarte() )
+	|	if int = 0 ( passerTour() )
+	
+	|_Boucle tant que passerTour = False OU typeCarte = Active OU Main != Vide
+	*/
+}
