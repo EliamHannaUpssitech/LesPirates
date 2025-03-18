668d9f99eb4113e456b5f89b0a8ba98e21eaeff1
EliamHannaUpssitech <eliam.hanna@univ-tlse3.fr>
2025-03-18 11:55:13 +0100

    Ajout de la carte Roulette (pas implementée encore)

---
+++ src/effets/Roulette.java
@@ -0,0 +1,17 @@
+package effets;
+
+import cartes.Carte;
+import cartes.Effet;
+import jeu.Joueur;
+
+public class Roulette extends Carte {
+
+	public Roulette(Effet effet) {
+		super(effet);
+	}
+	
+	@Override
+	public void affectEffet(Joueur joueurUtil, Joueur joueurAdv) {
+		
+	}
+}
